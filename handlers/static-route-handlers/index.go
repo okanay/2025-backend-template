0753603332b1118f450cc8fb682a4013a09699a7
Okan Ay <okanay@hotmail.com>
2025-07-22 00:55:23 +0300

    test version 4

--- handlers/static-route-handlers/index.go
+++ handlers/static-route-handlers/index.go
@@ -12,6 +12,6 @@ func (h *Handler) Index(c *gin.Context) {
 		"Framework": "Gin",
 		"Database":  "PostgreSQL",
 		"Status":    "System is running successfully.",
-		"Version":   "1.0.2",
+		"Version":   "1.0.3",
 	})
 }
